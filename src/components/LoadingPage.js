ff272d8380c040b1bb0c12c04211edf7c1420951
deepak-rasamsetty <deepakrasamsetty92@gmail.com>
2023-01-12 21:08:17 +0530

    Updated icon in loadingPage

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -4,7 +4,7 @@ export default function LoadingPage() {
   return (
     <div class=" d-flex justify-content-center align-items-center" style={{height:'100vh'}}>
   <div className='text-center'>
-  <i class="bi bi-radioactive"></i>
+ <i class="bi bi-clock"></i>
      <h5>Please wait while we validate the payment</h5>
      <figcaption>Page will automatically redirect to confirmation page once done</figcaption>
     
